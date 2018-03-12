10648793f8aa819053e80386ef27ddea46e8b9e2
Rogério_Santos - Agência Webforsite <contato@webforsite.com.br>
2018-03-12 13:34:26 -0300

    Create app.php

---
+++ app.php
@@ -0,0 +1,32 @@
+<?php
+
+function header_ajax()
+{
+$url_atual = "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+$urlblock = "http://leads.epizy.com/sistema/cadastrar-campanha/";
+$urlblock_2 = "http://leads.epizy.com/sistema/teste";
+if($url_atual == $urlblock || $url_atual == $urlblock_2):
+       echo '<!-- sem script -->';
+else:
+       echo wp_head();
+endif;
+
+}
+
+// parte 2 do teste
+
+function logo(){
+
+$url_atual = "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+$url2 = "http://leads.epizy.com/sistema/cadastrar-campanha/";
+$url3 = "http://leads.epizy.com/sistema/campanhas/";
+if($url_atual == $url2 || $url_atual == $url3):
+       echo ' "/#" ';
+else:
+       echo esc_url( home_url( '/' ) );
+endif;
+}
+
+
+
+
