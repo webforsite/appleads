8b16c3047df2fdbd00868eef45c215f0ab998a3b
Rogério_Santos - Agência Webforsite <contato@webforsite.com.br>
2018-04-01 21:35:32 -0300

    Add files via upload

--- usuarios.php
+++ usuarios.php
@@ -1,26 +1,42 @@
 <?php
 
+$pdo = new PDO("mysql:host=sql106.epizy.com; dbname=epiz_21528787_wp27", "epiz_21528787", "012714");
+$pdo->exec("set names utf8"); 
+$sql = "SELECT * FROM wpe4_users ";
+$result = $pdo->query( $sql );
+$rows = $result->fetchAll( PDO::FETCH_ASSOC );
+$stmt = $pdo->prepare($sql);
+$stmt->execute();
+$result = $stmt->fetchAll(PDO::FETCH_OBJ);
+$html  ="<table class='table'>";
+$html .= "<thead>
+<tr>
+    <th>ID</th>
+    <th>USER_LOGIN</th>
+    <th>USER_NICENAME</th>
+    <th>USER_EMAIL</th>
+    <th>USER_REGISTERED</th>
+    <th>DISPLAY_NAME</th>
+    
+</tr>
+</thead>";
 
-#super admin
-$rogerio = "net.sales0001@gmail.com";
-$liane = "lia@webforsite.com.br";
-$admin ="admin@leads.epizy.com";
+$html .="<tbody>";
+for($i = 0; $i < count($result); $i++){
 
-# *** EMPRESAS***
-
-// func roles empresas //
-
-# adm 
-# supervisores
-# consultores
-
-// *** LISTA DE CONSULTORES *** //
-
-$liego = "liego@webforsite.com.br";
-
-//
+$html .= "<tr>";
+$html .= "<td>". $result[$i]->ID ."</td>";
+$html .= "<td>". $result[$i]->user_login ."</td>";
+$html .= "<td>". $result[$i]->user_nicename ."</td>";
+$html .= "<td>". $result[$i]->user_email ."</td>";
+$html .= "<td>". $result[$i]->user_registered ."</td>";
+$html .= "<td>". $result[$i]->display_name ."</td>";
 
 
+$html .= "</tr>";
+}
+$html .= "</tbody>
+</table>";
+echo $html;
 
-
-
+?>
